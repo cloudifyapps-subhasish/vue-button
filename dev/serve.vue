fba8e2acd7898eb16aa6f133655dde462ccb0afa
cloudifyapps-subhasish <subhasish@cloudifyapps.com>
2022-10-17 13:08:30 +0530

    Initial vue Button

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import SgVueButton from '@/sg-vue-button.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    SgVueButton
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <sg-vue-button />
+  </div>
+</template>
